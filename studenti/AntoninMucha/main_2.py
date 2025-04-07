08163f16cf12465b89c833ef368030146d1785dd
AntoninMucha <ant.muc@sosjablunkov.cz>
2025-04-07 10:39:13 +0200

    Úprava vol.2

--- studenti/AntoninMucha/main_2.py
+++ studenti/AntoninMucha/main_2.py
@@ -1,1 +1,27 @@
-#test
+class Animal:
+    def __init__(self, name):
+        self.name = name
+
+    def make_sound(self):
+        return "Some generic animal sound"
+
+class Dog(Animal):
+    def __init__(self, name, breed):
+        super().__init__(name)
+        self.breed = breed
+
+    def make_sound(self):
+        return "Woof!"
+
+class Cat(Animal):
+    def __init__(self, name):
+        super().__init__(name)
+
+    def make_sound(self):
+        return "Meow!"
+
+my_dog = Dog("Buddy", "Golden Retriever")
+my_cat = Cat("Whiskers")
+
+print(my_dog.make_sound())
+print(my_cat.make_sound())  
